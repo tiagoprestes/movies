8a2e76b83279ea8e96c9ad642cc5d543d5afc675
prestes <tiagoprestesdev@gmail.com>
2023-08-09 07:32:04 -0300

    commit

--- app/src/main/java/com/example/movies/MovieListResponse.kt
+++ app/src/main/java/com/example/movies/MovieListResponse.kt
@@ -5,5 +5,4 @@ import com.google.gson.annotations.SerializedName
 
 data class MovieListResponse(
     @SerializedName("results") val results: List<Movie>,
-    // Other properties from the response can be added here
 )
--- app/src/main/java/com/example/movies/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/movies/adapter/MovieAdapter.kt
@@ -4,8 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movies.R
 import com.example.movies.model.Movie
@@ -21,15 +19,12 @@ class MovieAdapter(
 
     // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_movie, parent, false)
 
         return ViewHolder(view)
     }
 
-    // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val model = mList.get(position)
@@ -40,17 +35,14 @@ class MovieAdapter(
 
         val ItemsViewModel = mList[position]
 
-        // sets the text to the textview from our itemHolder class
         holder.title.text = ItemsViewModel.title
 
     }
 
-    // return the number of the items in the list
     override fun getItemCount(): Int {
         return mList.size
     }
 
-    // Holds the views for adding it to image and text
     inner class ViewHolder(
         ItemView: View
     ) : RecyclerView.ViewHolder(ItemView) {
